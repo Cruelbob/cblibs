357c61b8084d1586ed76c4aec6c068cb1042cff6
Cruelbob <vlad.kolotvin@gmail.com>
2013-07-26 15:40:14 +0400

    Tabs replaced by spaces

--- src/protocol/example/echo_example/echo_client.cpp
+++ src/protocol/example/echo_example/echo_client.cpp
@@ -11,7 +11,7 @@ int main(int argc,char** argv) {
     try {
         cb::protocol::client mc(io_service);
         mc.set_on_packet(msgs::s2c::echo,[&](const std::string& str) {
-			std::cout << "from server: " << str << std::endl;
+            std::cout << "from server: " << str << std::endl;
             mc.close();
         });
         auto con_res = mc.connect("127.0.0.1",1234);
@@ -19,8 +19,8 @@ int main(int argc,char** argv) {
             return io_service.run();
         });
         if(con_res.get() == 0) {
-			std::string str = argc > 1 ? argv[1] : "hello";
-			std::cout << "to server: " << str << std::endl;
+            std::string str = argc > 1 ? argv[1] : "hello";
+            std::cout << "to server: " << str << std::endl;
             mc.send(msgs::c2s::echo,str);
         }
         con_thread.get();
